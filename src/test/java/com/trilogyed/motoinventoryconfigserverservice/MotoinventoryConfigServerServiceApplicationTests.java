3179ce8fce7cafc33200f83f853030095d2b6425
cynics <cormil@gmail.com>
2019-08-18 07:59:03 -0500

    initial config server setup

---
+++ src/test/java/com/trilogyed/motoinventoryconfigserverservice/MotoinventoryConfigServerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trilogyed.motoinventoryconfigserverservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MotoinventoryConfigServerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
