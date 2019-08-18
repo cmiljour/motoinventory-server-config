94c64c18ae0098d74cfbd0234f26fd5aab7532d3
cormil@gmail.com <cormil@gmail.com>
2019-08-18 12:01:05 -0500

    added moto client

---
+++ motoinventory-eureka-server-service/src/main/java/com/trilogyed/motoinventoryeurekaserverservice/MotoinventoryEurekaServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.motoinventoryeurekaserverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoinventoryEurekaServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoinventoryEurekaServerServiceApplication.class, args);
+	}
+
+}
